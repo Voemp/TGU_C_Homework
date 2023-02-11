ce9380786fb281c9e00b014ff52505ca013bdd69
Void_Empty <87643822+Voemp@users.noreply.github.com>
2023-02-11 17:38:32 +0800

    Delete exp2_5.c

--- exp2_5.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-int main() {
-    float a, b, c;
-    scanf("%f%f%f", &a, &b, &c);
-    printf("%.0f %.0f", (-b + sqrt(b * b - 4 * a * c)) / 2 / a, (-b - sqrt(b * b - 4 * a * c)) / 2 / a);
-    return 0;
-}
